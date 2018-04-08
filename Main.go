a9c7b6253ecbd7526c60e9c3f209c14aa8a5b0f5
DuffyTJ89 <tomduffy89@gmail.com>
2018-04-08 16:17:17 +0100

    + not working, missing something, giving back false when true expected

--- Main.go
+++ Main.go
@@ -102,13 +102,13 @@ func poregtonfa(pofix string) *nfa { //return pointer to nfa
 			nfaStack = append(nfaStack, &nfa{initial: &initial, accept: &accept}) //push new frag to nfa stack
 
 		case '+':
+			//like * but dont need to accept the zero, looking for one or more
 			//only pop one frag off the nfa stack
 			frag := nfaStack[len(nfaStack)-1]
-			nfaStack = nfaStack[:len(nfaStack)-1]
 
 			accept := state{}
 			initial := state{edge1: frag.initial, edge2: &accept}
-			frag.accept.edge1 = frag.initial
+			frag.accept.edge1 = &initial
 
 			nfaStack = append(nfaStack, &nfa{initial: &initial, accept: &accept}) //push new frag to nfa stack
 
